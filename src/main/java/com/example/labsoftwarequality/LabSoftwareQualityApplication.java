2c67f568eba35760ffcdd7d9d620e03c7b6551cd
Driss <drissaitkassi@gmail.com>
2023-08-12 14:21:12 +0100

    first commit

---
+++ src/main/java/com/example/labsoftwarequality/LabSoftwareQualityApplication.java
@@ -0,0 +1,13 @@
+package com.example.labsoftwarequality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSoftwareQualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSoftwareQualityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labsoftwarequality/LabSoftwareQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labsoftwarequality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSoftwareQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
